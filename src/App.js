765897e223ef46df23dc3b02f0696fc54c9b3157
Thao Tran <thaotran@Thaos-MacBook-Pro-2.local>
2019-10-06 19:46:19 +0200

    cleaner initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import profil from "./bilde.jpg";
 import "./App.css";
 import Link from "./Link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   let teller = 0;
@@ -11,15 +13,24 @@ function App() {
       <header className="App-header">
         <img src={profil} className="App-logo" alt="logo" />
         <p>Thanh Thao Thi Tran</p>
-        {/* {"Antall kjærester: " + (teller + 5)} */}
 
-        {/* Legger til linker */}
-        <Link
-          link={"https://www.linkedin.com/in/thanh-thao-thi-tran-27367a15b/"}
-        >
-          LinkedIn
-        </Link>
-        <Link link={"https://github.com/ThanhThaoThiTran"}>Github</Link>
+        <div class="flex-container">
+          <Link
+            link={"https://www.linkedin.com/in/thanh-thao-thi-tran-27367a15b/"}
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+            LinkedIn
+          </Link>
+
+          {/* {"Antall kjærester: " + (teller + 5)} */}
+
+          {/* Legger til linker */}
+
+          <Link link={"https://github.com/ThanhThaoThiTran"}>
+            <FontAwesomeIcon icon={faGithub} />
+            Github
+          </Link>
+        </div>
       </header>
     </div>
   );
